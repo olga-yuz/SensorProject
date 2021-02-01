7ddcd7745c672bab9e7b58b6608797b64df37a35
olga-yuz <oyuzenkova@academytrainee.com>
2021-02-01 12:12:21 +0000

    added library

---
+++ VehicleLib/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VehicleLib
+{
+    public class Vehicle
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int vehicleId { get; set; }
+        public int temp { get; set; }
+        public int humidity { get; set; }
+    }
+}
