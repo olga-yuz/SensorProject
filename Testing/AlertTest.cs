e1d852acfb123ff300957c7731f9e0f69044ec41
wdakora <wdakora@academytrainee.com>
2021-02-03 16:15:42 +0000

    Test api

---
+++ Testing/AlertTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleAPI.Controllers;
+using VehicleLib;
+using VehicleLib.Interface;
+
+namespace Testing
+{
+   public class AlertTest
+    {
+        private Mock<ILogger<RedAlertController>> _logger;
+        private Mock<IRepositoryWrapper> mockRepo;
+        private RedAlertController vehicleController;
+        private AddAlert addAlert;
+        private UpdateAlert updateAlert;
+        private RedAlert redAlert;
+        private List<RedAlert> alerts;
+        private Mock<IRedAlert> alertMock;
+        private List<IRedAlert> alertsMock;
+        private Mock<IAddAlert> addAlertMock;
+        private Mock<IUpdateAlert> updateAlertMock;
+        private Mock<IAlertViewModel> alertViewModelMock;
+        private List<IAlertViewModel> alertssViewModelMock;
+
+/*        public AlertTest()
+        {
+            //mock setup
+            vehicleMock = new Mock<IVehicle>();
+            vehiclesMock = new List<IVehicle> { vehicleMock.Object };
+            addVehicleMock = new Mock<IAddVehicle>();
+            updateVehicleMock = new Mock<IUpdateVehicle>();
+            vehicle = new Vehicle();
+            vehicles = new List<Vehicle>();
+            //viewmodels mock setup
+            vehicleViewModelMock = new Mock<IVehicleViewModel>();
+            vehiclesViewModelMock = new List<IVehicleViewModel>();
+
+            //sample models
+            addVehicle = new AddVehicle { temp = 22, humidity = 58 };
+            updateVehicle = new UpdateVehicle { temp = 34, humidity = 67 };
+
+            //controller setup
+
+            _logger = new Mock<ILogger<VehicleController>>();
+
+            mockRepo = new Mock<IRepositoryWrapper>();
+
+            var allVehicles = GetVehicles();
+            vehicleController = new VehicleController(_logger.Object, mockRepo.Object);
+        }
+*/
+    }
+}
--- Testing/VehicleTest.cs
+++ Testing/VehicleTest.cs
@@ -1,14 +1,150 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
 using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using VehicleAPI.Controllers;
+using VehicleLib;
+using VehicleLib.Interface;
 using Xunit;
 
 namespace Testing
 {
-    public class UnitTest1
+    public class VehicleControllerTest
     {
-        [Fact]
-        public void Test1()
-        {
+        private Mock<ILogger<VehicleController>> _logger;
+        private Mock<IRepositoryWrapper> mockRepo;
+        private VehicleController vehicleController;
+        private AddVehicle addVehicle;
+        private AddVehicle addExistingVehicle;
+        private UpdateVehicle updateVehicle;
+        private Vehicle vehicle;
+        private List<Vehicle> vehicles;
+        private Mock<IVehicle> vehicleMock;
+        private List<IVehicle> vehiclesMock;
+        private Mock<IAddVehicle> addVehicleMock;
+        private Mock<IUpdateVehicle> updateVehicleMock;
+        private Mock<IVehicleViewModel> vehicleViewModelMock;
+        private List<IVehicleViewModel> vehiclesViewModelMock;
 
+        public VehicleControllerTest()
+        {
+            //mock setup
+            vehicleMock = new Mock<IVehicle>();
+            vehiclesMock = new List<IVehicle> { vehicleMock.Object };
+            addVehicleMock = new Mock<IAddVehicle>();
+            updateVehicleMock = new Mock<IUpdateVehicle>();
+            vehicle = new Vehicle();
+            vehicles = new List<Vehicle>();
+            //viewmodels mock setup
+            vehicleViewModelMock = new Mock<IVehicleViewModel>();
+            vehiclesViewModelMock = new List<IVehicleViewModel>();
+
+            //sample models
+            addVehicle = new AddVehicle { temp = 22, humidity = 58 };
+            addExistingVehicle = new AddVehicle { temp = 22, humidity = 58 };
+            updateVehicle = new UpdateVehicle { temp = 34, humidity = 67 };
+
+
+            //controller setup
+            
+            _logger = new Mock<ILogger<VehicleController>>();
+           
+            mockRepo = new Mock<IRepositoryWrapper>();
+
+            var allVehicles = GetVehicles();
+            vehicleController = new VehicleController(_logger.Object, mockRepo.Object);
+        }
+
+        [Fact]
+        public void GetAllVehiclesTest()
+        {
+            //Arrange
+            mockRepo.Setup(repo => repo.Vehicles.FindAll()).Returns(GetVehicles());
+            mockRepo.Setup(repo => repo.Vehicles.FindByCondition(r => r.vehicleId == It.IsAny<int>())).Returns(GetVehicles());
+            //Act
+            var controllerActionResult = vehicleController.Get();
+            //Assert
+            Assert.NotNull(controllerActionResult);
+        }
+
+        [Fact]
+        public void AddVehicleTest()
+        {
+            //Arrange
+            mockRepo.Setup(repo => repo.Vehicles.FindByCondition(c => c.vehicleId == It.IsAny<int>())).Returns(GetVehicles());
+            //Act
+            var controllerActionResult = vehicleController.Post(addVehicle);
+            //Assert
+            Assert.NotNull(controllerActionResult);
+            Assert.IsType<ActionResult<VehicleViewModel>>(controllerActionResult);
+        }
+
+        [Fact]
+        public void AddExistingVehicleTest()
+        {
+            //Arrange
+            mockRepo.Setup(repo => repo.Vehicles.FindByCondition(c => c.vehicleId == It.IsAny<int>())).Returns(GetVehicles());
+            //Act
+            var controllerActionResult = vehicleController.Post(addExistingVehicle);
+            //Assert
+            Assert.NotNull(controllerActionResult);
+            Assert.IsType<ActionResult<VehicleViewModel>>(controllerActionResult);
+        }
+
+        [Fact]
+        public void DeleteVehicleTest()
+        {
+            //Arrange
+            mockRepo.Setup(repo => repo.Vehicles.FindByCondition(r => r.vehicleId == It.IsAny<int>())).Returns(GetVehicles());
+            mockRepo.Setup(repo => repo.Vehicles.Delete(GetVehicle()));
+            //Act
+            var controllerActionResult = vehicleController.Delete(It.IsAny<int>());
+            //Assert
+            Assert.NotNull(controllerActionResult);
+        }
+
+        [Fact]
+        public void UpdateVehicleTest()
+        {
+            //Arrange
+            mockRepo.Setup(repo => repo.Vehicles.FindByCondition(c => c.vehicleId == It.IsAny<int>())).Returns(GetVehicles());
+            mockRepo.Setup(repo => repo.Vehicles.Update(GetVehicle()));
+            //Act
+            var controllerActionResult = vehicleController.Put(It.IsAny<int>(),updateVehicle);
+            //Assert
+            Assert.NotNull(controllerActionResult);
+            Assert.IsType<ActionResult<VehicleViewModel>>(controllerActionResult);
+            Assert.Equal(updateVehicle, updateVehicle);
+        }
+
+
+        [Fact]
+        public void GetOneVehicleTest()
+        {
+            //Arrange
+  
+            mockRepo.Setup(repo => repo.Vehicles.FindByCondition(r => r.vehicleId == It.IsAny<int>())).Returns(GetVehicles());
+            //Act
+            var controllerActionResult = vehicleController.Get(It.IsAny<int>());
+            //Assert
+            Assert.NotNull(controllerActionResult);
+           
+        }
+
+        private IEnumerable<Vehicle> GetVehicles()
+        {
+            var vehicles = new List<Vehicle> {
+            new Vehicle(){vehicleId=1,temp = 22, humidity = 58 },
+            new Vehicle(){vehicleId=1, temp = 222, humidity = 582 }
+            };
+            return vehicles;
+        }
+        private Vehicle GetVehicle()
+        {
+            return GetVehicles().ToList()[0];
         }
     }
 }
